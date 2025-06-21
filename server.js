979936506f7dc57302421373f956066f88c1ba15
Deepak <testdeveloper278@gmail.com>
2025-06-21 21:48:04 +0530

    Removed folder from repository

--- server.js
+++ server.js
@@ -10,4 +10,12 @@ const port=process.env.PORT || 5000
 
 app.listen(port,()=>console.log(`server is runnning on port ${port}`));
 
+app.use("/api/users",require("./route/api/users"));
+
+app.use("/api/auth",require("./route/api/auth"));
+
+app.use("/api/posts",require("./route/api/posts"));
+
+app.use("/api/profile",require("./route/api/profile"));
+
 app.get("/",(req,res)=>res.send("API is running"));
