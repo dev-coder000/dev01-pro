11301cfe95e738fe196c1990e9424107637608c7
dd05123 <deepakdogra@revinfotech.in>
2025-07-12 19:55:32 +0530

    profile

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require("express");
+
+const connect=require("./config/db");
+
+const app=express();
+
+connect();
+
+const port=process.env.PORT || 5000
+
+app.listen(port,()=>console.log(`server is runnning on port ${port}`));
+
+app.use(express.json({extended:true}))
+
+app.use("/api/users",require("./route/api/users"));
+
+app.use("/api/auth",require("./route/api/auth"));
+
+app.use("/api/posts",require("./route/api/posts"));
+
+app.use("/api/profile",require("./route/api/profile"));
+
+app.get("/",(req,res)=>res.send("API is running"));
